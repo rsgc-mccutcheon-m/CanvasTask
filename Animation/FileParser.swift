48ea5f2c4c55d10782dc604ce884d1dc62910185
Mark McCutcheon <mmccutcheon@rsgc.on.ca>
2017-05-29 22:08:06 -0400

    Decided to move on from branching and try to get file read/parse working 'cause branching is broken in a subtle and odd way I can't trace as of yet.

---
+++ Animation/FileParser.swift
@@ -0,0 +1,33 @@
+//
+//  FileParser.swift
+//  Canvas
+//
+//  Created by Jarvis on 2017-05-29.
+//  Copyright © 2017 Royal St. George's College. All rights reserved.
+//
+
+import Foundation
+
+public class FileParser{
+    
+    
+    
+    let path : String
+    
+    init(path: String) {
+        self.path = path
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
